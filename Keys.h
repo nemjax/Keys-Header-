1538184ddf4d062af51ef7d681c647cfbe3068ce
nemjax <aycakckdrmz@gmail.com>
2016-02-16 16:46:55 -0500

    Keys Header
    It is gonna define every key in all in once once it is included. It is
    also way easier to make changes, add moree..

---
+++ Keys.h
@@ -0,0 +1,51 @@
+//
+//  Keys.h
+//  
+//
+//  Created by AYÇA KÜÇÜKDURMAZ on 2/16/16.
+//
+//
+
+#ifndef Keys_h
+#define Keys_h      //just include this header this way you'll have the conrol of most keys
+                    //if you want more all you need define here it's good for usefulness
+#define ENTER 10    //all hard codes for the keys are like variables now.
+#define TAB 9
+#define SPACE 32
+#define ESC 27
+
+#define LEFT 260
+#define RIGHT 261
+#define UP 259
+#define DOWN 258
+
+#define BACKSPACE 263
+
+// ControlCombos
+#define CTRLH 8
+#define CTRLV 22
+#define CTRLX 24
+#define CTRLO 15
+#define CTRLL 12
+#define CTRLY 25
+
+
+//not sure if we will need this
+#define INS 331
+#define HOME 262
+#define PGUP 339
+
+//lastly maybe if we go further and need it some F keys
+#define F1 265
+#define F2 266
+#define F3 267
+#define F4 268
+#define F5 269
+
+
+
+
+
+
+
+#endif /* Keys_h */
